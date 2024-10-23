892e24d7a7e92dc0c4c478ba968e1390d790ccb6
gervais <akoinenzegervais7@gmail.com>
2024-10-23 22:50:41 +0000

    4 commit

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useCallback, useRef } from 'react';
 import '../pages/navbar.css';
 import { IoPersonAddSharp } from "react-icons/io5";
 import { GiPadlock } from "react-icons/gi";
-import { FaUserCircle } from "react-icons/fa"; // Assurez-vous que l'importation est correcte
+import { FaUserCircle, FaBars } from "react-icons/fa"; // Importation de l'icône de menu
 import { Link } from 'react-router-dom';
 import Footer from '../pages/Footer';
 
@@ -48,59 +48,84 @@ const Navbar = () => {
   };
 
   const handleDropdownToggle = () => {
-    setDropdownVisible(prevState => !prevState); // Ajout de cette fonction
+    setDropdownVisible(prevState => !prevState);
   };
 
   return (
     <>
       <header className="header-area overlay">
         <nav ref={myRef} className={`navbar navbar-expand-md navbar-dark ${isFixed ? 'fixed-top' : ''}`}>
-        <div className="container">
-  <a href="" className="navbar-brand">E-AFEP</a>
+          <div className="container">
+            <a href="/" className="navbar-brand">E-AFEP</a>
 
-  <button
-    type="button"
-    className="navbar-toggler collapsed"
-    onClick={handleNavToggle}
-    aria-label="Toggle navigation"
-  >
-    <span className="menu-icon-bar"></span>
-    <span className="menu-icon-bar"></span>
-    <span className="menu-icon-bar"></span>
-  </button>
+            {/* Bouton pour ouvrir l'offcanvas sur mobile avec icône de menu */}
+            {windowWidth < 768 && (
+              <button
+                className="btn"
+                type="button"
+                data-bs-toggle="offcanvas"
+                data-bs-target="#offcanvasRight"
+                aria-controls="offcanvasRight"
+                style={{ backgroundColor: 'black', color: 'white' }} // Couleur de fond et couleur du texte
+              >
+                <FaBars style={{ fontSize: '24px' }} />
+              </button>
+            )}
 
-  <div id="main-nav" className={`collapse navbar-collapse ${navVisible ? 'show' : ''}`}>
-    <ul className="navbar-nav ms-auto">
-      {windowWidth >= 768 && (
-        <li>
-          <Link to="/guide" className="nav-item nav-link">
-            Guide utilisateur
-          </Link>
-        </li>
-      )}
+            {/* Offcanvas menu uniquement visible sur mobile */}
+            {windowWidth < 768 && (
+  <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel" style={{backgroundColor:"#20247b"}}>
+    <div className="offcanvas-header">
+      <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div className="offcanvas-body  "  >
+      <ul className="navbar-nav flex-column" style={{backgroundColor:"#20247b"}}> {/* Menu vertical */}
+        <li className={`nav-item dropdown ${dropdownVisible ? 'show' : ''}`} >
+          <a
+            href="#"
+            className="nav-link dropdown-toggle"
+            onClick={handleDropdownToggle}
+            aria-haspopup="true"
+            aria-expanded={dropdownVisible}
 
-      <li className={`dropdown ${dropdownVisible ? 'show' : ''}`}>
-        <a 
-          href="#" 
-          className="nav-item nav-link dropdown-toggle" 
-          onClick={handleDropdownToggle} 
-          aria-haspopup="true" 
-          aria-expanded={dropdownVisible}
-        >
-          <FaUserCircle style={{ marginRight: 5 }} /> Mon espace E-AFEP
-        </a>
-        <div className={`dropdown-menu ${dropdownVisible ? 'show' : ''}`}>
-          <Link to="/profils" className="dropdown-item">
-            <FaUserCircle style={{ marginRight: 5 }} /> Mon compte
-          </Link>
-          <a href="#" className="dropdown-item">
-            <GiPadlock style={{ marginRight: 5 }} /> Déconnexion
+          >
+            <FaUserCircle style={{ marginRight: 5 }} /> Mon espace E-AFEP
           </a>
-        </div>
-      </li>
-    </ul>
+          <div className={`dropdown-menu ${dropdownVisible ? 'show' : ''}`}>
+            <Link to="/profils" className="dropdown-item" style={{backgroundColor:"#20247b"}}>Mon compte</Link>
+            <a href="#" className="dropdown-item" style={{backgroundColor:"#20247b"}}>Déconnexion</a>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
-</div>
+)}
+            {/* Menu classique pour les écrans plus larges */}
+            <div id="main-nav" className={`collapse navbar-collapse ${navVisible ? 'show' : ''}`}>
+              <ul className="navbar-nav ms-auto">
+                {windowWidth >= 768 && (
+                  <li className="nav-item">
+                    <Link to="/guide" className="nav-link">Guide utilisateur</Link>
+                  </li>
+                )}
+                <li className={`nav-item dropdown ${dropdownVisible ? 'show' : '' }`}>
+                  <a
+                    href="#"
+                    className="nav-link dropdown-toggle"
+                    onClick={handleDropdownToggle}
+                    aria-haspopup="true"
+                    aria-expanded ={dropdownVisible}
+                  >
+                    <FaUserCircle style={{ marginRight: 5 }} /> Mon espace E-AFEP
+                  </a>
+                  <div className={`dropdown-menu ${dropdownVisible ? 'show' : ''}`}>
+                    <Link to="/profils" className="dropdown-item">Mon compte</Link>
+                    <a href="#" className="dropdown-item">Déconnexion</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
         </nav>
 
         <div className="banner">
@@ -119,56 +144,56 @@ const Navbar = () => {
         </div>
       </header>
 
-<main className="d-none d-lg-block">
-    <section className="section services-section" id="services">
-        <div className="container">
+      <main className="d-none d-lg-block">
+        <section className="section services-section" id="services">
+          <div className="container">
             <div className="row">
-                <div className="col-lg-6">
-                    <div className="section-title">
-                        <h2>Pourquoi E-AFEP ?</h2>
-                    </div>
+              <div className="col-lg-6">
+                <div className="section-title">
+                  <h2>Pourquoi E-AFEP ?</h2>
                 </div>
+              </div>
             </div>
             <div className="row">
-                <div className="col-sm-6 col-lg-4">
-                    <div className="feature-box-1">
-                        <div className="icon">
-                            <i className="fa fa-user"></i>
-                        </div>
-                        <div className="feature-content">
-                            <h5>Simplicité</h5>
-                            <p>Un seul espace personnel pour tout faire et tout suivre en temps réel.</p>
-                        </div>
-                    </div>
+              <div className="col-sm-6 col-lg-4">
+                <div className="feature-box-1">
+                  <div className="icon">
+                    <i className="fa fa-user"></i>
+                  </div>
+                  <div className="feature-content">
+                    <h5>Simplicité</h5>
+                    <p>Un seul espace personnel pour tout faire et tout suivre en temps réel.</p>
+                  </div>
                 </div>
-                <div className="col-sm-6 col-lg-4">
-                    <div className="feature-box-1">
-                        <div className="icon">
-                            <i className="fa fa-key"></i>
-                        </div>
-                        <div className="feature-content">
-                            <h5>Sécurité</h5>
-                            <p>Nous sécurisons et fiabilisons vos informations.</p>
-                        </div>
-                    </div>
+              </div>
+              <div className="col-sm-6 col-lg-4">
+                <div className="feature-box-1">
+                  <div className="icon">
+                    <i className="fa fa-key"></i>
+                  </div>
+                  <div className="feature-content">
+                    <h5>Sécurité</h5>
+                    <p>Nous sécurisons et fiabilisons vos informations.</p>
+                  </div>
                 </div>
-                <div className="col-sm-6 col-lg-4">
-                    <div className="feature-box-1">
-                        <div className="icon">
-                            <i className="fa fa-hourglass"></i>
-                        </div>
-                        <div className="feature-content">
-                            <h5>Gain De Temps</h5>
-                            <p>Plus besoin de se déplacer : faites vos démarches quand vous voulez.</p>
-                        </div>
-                    </div>
+              </div>
+              <div className="col-sm-6 col-lg-4">
+                <div className="feature-box-1">
+                  <div className="icon">
+                    <i className="fa fa-hourglass"></i>
+                  </div>
+                  <div className="feature-content">
+                    <h5>Gain De Temps</h5>
+                    <p>Plus besoin de se déplacer : faites vos démarches quand vous voulez.</p>
+                  </div>
                 </div>
+              </div>
             </div>
-        </div>
-    </section>
-</main>
+          </div>
+        </section>
+      </main>
 
-<Footer className="d-none d-lg-block" />
+      <Footer className="d-none d-lg-block" />
     </>
   );
 };
