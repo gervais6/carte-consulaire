d2e1b0e76f1178dc7dbfc2b8757ebfca83a44648
Gervais6 <102412053+gervais6@users.noreply.github.com>
2024-12-14 21:31:57 +0000

    ajout des icon et interminet Auth

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -8,24 +8,7 @@ const Navbar = () => {
     <div>
       <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" />
 
-      <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        <div className="container">
-          <a className="navbar-brand" href="#">E-afep</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="#features">Caractéristiques</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#about">À propos</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+
 
       <header className="hero">
         <div className="container">
